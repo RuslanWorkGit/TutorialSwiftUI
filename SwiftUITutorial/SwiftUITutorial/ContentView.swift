5c31f6fc25d09313365782e60c40a22cdec1ceab
RuslanWorkGit <lulka758@gmail.com>
2024-12-04 22:36:40 +0200

    end section 1 and 2

---
+++ SwiftUITutorial/SwiftUITutorial/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  SwiftUITutorial
+//
+//  Created by Ruslan Liulka on 04.12.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Turtle Rock")
+            .font(.title)
+            
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUITutorial/SwiftUITutorial/SwiftUITutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUITutorialApp.swift
+//  SwiftUITutorial
+//
+//  Created by Ruslan Liulka on 04.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUITutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
